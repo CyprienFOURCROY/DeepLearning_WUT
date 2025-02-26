5aca9e55ca80a880f9d02f480519e9170c21fdae
Cyprien Fourcroy <cyprien.fourcroy@student-cs.fr>
2025-02-26 16:01:18 +0100

    folder organization

---
+++ models/architecture/resnet_18.py
@@ -0,0 +1,14 @@
+import torch.nn as nn
+
+
+class Alex_Net(nn.Module):
+
+    def __init__(self, num_classes: int = 1000) -> None:
+        super(Alex_Net, self).__init__()
+
+        self.features = nn.Sequential(
+            
+        )
+
+
+
